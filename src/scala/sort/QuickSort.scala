dd2168ab1e08f1405c6ab27bc9c22cb2720d2a28
tk <tk.kim@daumkakao.com>
2016-12-03 19:28:19 +0900

    quickstep #1

---
+++ src/scala/sort/QuickSort.scala
@@ -0,0 +1,25 @@
+package scala.sort
+
+import scala.util.Random
+
+/**
+  * Created by bistros on 2016. 12. 3..
+  *
+  * quicksort
+  */
+object QuickSort extends App {
+
+
+  def qs1(list: List[Int]) : List[Int] = list match {
+    case Nil => Nil
+    case a :: tail =>
+      val (less,grater) = tail.partition( _ <= a)
+      qs1(less) ::: List(a) ::: qs1(grater)
+
+  }
+
+
+  println("qs1 : " + qs1(Seq.fill(10)(Random.nextInt(100) ).toList))
+
+
+}
