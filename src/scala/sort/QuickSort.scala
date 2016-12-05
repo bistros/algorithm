73701bee1863e8e951c2b66c19e77db837bfd8bd
tk <tk.kim@daumkakao.com>
2016-12-05 13:21:20 +0900

    quicksort 참고내용 추가

--- src/scala/sort/QuickSort.scala
+++ src/scala/sort/QuickSort.scala
@@ -1,11 +1,19 @@
-package scala.sort
+package sort
 
 import scala.util.Random
 
 /**
-  * Created by bistros on 2016. 12. 3..
+  * created by bistros on 2016. 12. 3..
   *
-  * quicksort
+  * quicksort ( https://en.wikipedia.org/wiki/quicksort#formal_analysis )
+  *
+  * best : o(n log n)
+  * worst : o(n^2) 으로 pivot에 의해 파티션이 (n-1, 1) 일 경우이다.
+  *
+  * 참고
+  * - 결국엔 pivot을 잘 뽑으면(가운대 숫자로) 더 성능이 좋아 진다 이것을 위한 방법으로 pivot을 head, tail, center로 뽑는게 아니라
+  *   임의대로 3개 뽑아 그 중 가운대 값을 쓰는 방법도 있다 (물론 n이 클 경우...)
+  * - Java1.7에는 DualPivotQuicksort 를 사용한다
   */
 object QuickSort extends App {
 
